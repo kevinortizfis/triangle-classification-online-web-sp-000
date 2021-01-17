662028643bc0ce7c53e50942de7020c05a427030
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-17 23:37:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
       :equilateral
     elsif side_1 != side_2 && side_2 != side_3
       :scalene
-    elsif
+    elsif sides.uniq.length == 2
       :isosceles
     else
       begin
