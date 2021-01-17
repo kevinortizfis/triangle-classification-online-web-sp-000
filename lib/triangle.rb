bac233519111cee69370e0e35128a3b31a79e5c1
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-17 23:12:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,14 +7,16 @@ class Triangle
   def kind
     @type
     if @type != (:equilateral||:isosceles||:scalene)
-
+      raise TriangleError
+    rescue TriangleError
+    end
   end
 
   class Triangle
   # triangle code
 
-  class TriangleError < StandardError
-    # triangle error code
+    class TriangleError < StandardError
+      # triangle error code
+    end
   end
-end
 end
