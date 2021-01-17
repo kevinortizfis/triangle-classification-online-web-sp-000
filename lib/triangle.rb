e4b17ca68122582f5c35ae8b640d013167c5ecc0
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-17 23:54:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   def kind
     if @sides.any?{|one| one <= 0}
       begin
-      raise TriangleError => error
+      raise TriangleError
         puts error.message
       rescue TriangleError
       end
